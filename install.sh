a1306d658c4f4a30735d0a4458207c5aae62b6c1
M. Gholam <mgholam@gmail.com>
2025-11-03 07:08:01 +0330

    - go snippet lsp - sane enter key insert mode

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ go install golang.org/x/tools/gopls@latest                            # LSP
 go install golang.org/x/tools/cmd/goimports@latest                    # Formatter
 go install github.com/nametake/golangci-lint-langserver@latest        # Linter
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest # Linter cl
-go install github.com/go-delve/delve/cmd/dlv@latest                   # Debugger
+#go install github.com/go-delve/delve/cmd/dlv@latest                   # Debugger
 
 # svelte
 npm i -g svelte-language-server
